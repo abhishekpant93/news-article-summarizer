212ff70ed3ebe4b1cc86d1595f2b035daed9513d
Abhishek Pant <abhishek.pant93@gmail.com>
2014-08-29 01:03:15 +0530

    filter unicode characters

--- scraper/nytimes_scraper/nytimes_scraper/spiders/nytimes_spider.py
+++ scraper/nytimes_scraper/nytimes_scraper/spiders/nytimes_spider.py
@@ -12,20 +12,36 @@ class NytimesSpider(CrawlSpider):
     start_urls = [
         "http://www.nytimes.com/"
     ]
-               
+
+    def filter_unicode(self, str):
+        str = str.replace("u'", "")
+        str = str.replace("\u201d","\"")
+        str = str.replace("\u201c", "\"")
+        str = str.replace("\u2019", "'")
+        str = str.replace("\u2018", "'")
+        str = str.replace("\u2014", " ")
+        str = str.replace("\'", "")
+        str = str.replace("\"", "")
+        return str
+        
     def parse(self, response):
         item = NytimesItem()
-        item['headline'] = str(response.xpath("//meta[@property='og:title']/@content").extract())
+        item['headline'] = self.filter_unicode(str(response.xpath("//meta[@property='og:title']/@content").extract())[1:-1])
         item['link'] = response.url
         text = ""
         i = 0
         num_pars = len(response.xpath('//*[@class="story-body-text story-content"]'))
+
         while i < num_pars:
-            text += str(response.xpath(('//*[@id="story"]/p[%d]/text()') % i).extract())
-            #print 'par: ' + text
+            par = str(response.xpath(('//*[@id="story"]/p[%d]/text()') % i).extract())[1:-1]
+            # print 'par_orig: ' + par
+            par = self.filter_unicode(par)
+            # print 'par_processed: ' + par
+            text += par
             i += 1
+            
         item['text'] = text
-        item['keywords'] = str(response.xpath("//meta[@name='news_keywords']/@content").extract())
+        item['keywords'] = self.filter_unicode(str(response.xpath("//meta[@name='news_keywords']/@content").extract())[1:-1])
 
         yield item
 
@@ -35,3 +51,4 @@ class NytimesSpider(CrawlSpider):
                 yield scrapy.Request(url, callback=self.parse)
 
         sleep(0.5)
+        
