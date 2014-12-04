e83dc04293326d5f0ba2b9a68454c26ba0dd6f98
Abhishek Pant <abhishek.pant93@gmail.com>
2014-12-05 00:51:19 +0530

    support serving of static files

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -81,6 +80,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Cache
 CACHES = {
