6c8241f3a4db1dbdeef641e1feb71c0b4fdf921b
Abhishek Pant <abhishek.pant93@gmail.com>
2014-12-05 01:19:20 +0530

    change static resource path

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,15 +76,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'tldr/static'),
 )
 
 # Cache
