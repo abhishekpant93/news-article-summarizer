131cf14bc1db5cfc954ed790df16175eafb1e689
Abhishek Pant <abhishek.pant93@gmail.com>
2014-12-05 22:44:20 +0530

    enable logging

--- app/app/settings.py
+++ app/app/settings.py
@@ -94,3 +94,36 @@ STATICFILES_DIRS = (
 
 # Session Engine
 SESSION_ENGINE = "django.contrib.sessions.backends.cache" 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'tldr.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'app': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
